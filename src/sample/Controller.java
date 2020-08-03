a8e985df959afe5f9f17573c1479faab0d6a1bee
Ivan <kiselev.ivan95@gmail.com>
2020-08-03 17:56:32 +0300

    added UI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.application.Application;
+import javafx.fxml.Initializable;
 
 public class Controller {
+   
+
 }
